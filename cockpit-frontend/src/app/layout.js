af91f981b6702ab20af8b61e727c583df54d15fd
Oke <56229919+onissen@users.noreply.github.com>
2023-10-19 17:19:49 +0200

    Aktueller Stand...

--- cockpit-frontend/src/app/layout.js
+++ cockpit-frontend/src/app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import '../../node_modules/northware-theme/style/index.sass'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +9,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="de">
+      <body>{children}</body>
     </html>
   )
 }
