ac74d294f1ecabfd26ff6eb9d0ad7fc58dd62eb3
Oke <56229919+onissen@users.noreply.github.com>
2023-10-27 13:48:47 +0200

    Tailwind Config does not support /app  anymore

--- cockpit-frontend/tailwind.config.js
+++ cockpit-frontend/tailwind.config.js
@@ -3,7 +3,6 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/theme/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
