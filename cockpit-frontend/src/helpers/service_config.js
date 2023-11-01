6840926988bf75c1bbbadfbc7c48fd1a76776a47
Oke <56229919+onissen@users.noreply.github.com>
2023-11-01 19:09:33 +0100

    Removed unused const service_mainnavigation

--- cockpit-frontend/src/helpers/service_config.js
+++ cockpit-frontend/src/helpers/service_config.js
@@ -1,13 +1,1 @@
-export const service_mainnavigation = [
-    {parent: {name: 'Home', href:'/'},
-        children: null
-    },
-    {parent: {name: 'Test', href:'/test'}, 
-        children: {
-            1: {name: 'Test3', href: '/test3'},
-            2: {name: 'Test4', href: '/test4'}
-        }
-    },
-]
-
 export const siteName = "Northware Cockpit"
