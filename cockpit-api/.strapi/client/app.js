fe6d306bcd69bb82fc5120bca5d5dd126b7c6c3a
Oke <56229919+onissen@users.noreply.github.com>
2023-12-01 17:57:01 +0100

    Build after update

--- cockpit-api/.strapi/client/app.js
+++ cockpit-api/.strapi/client/app.js
@@ -7,7 +7,10 @@ import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
+import customisations from "../../src/admin/app.js";
+
 renderAdmin(document.getElementById("strapi"), {
+  customisations,
   plugins: {
     graphql: graphql,
     i18n: i18N,
